3c89dd6f088505c2583930aa72a23ca9ed3c5d84
m0m0rzz <m0m0rzz@homeoffice.wal-mart.com>
2026-03-28 23:10:55 +0530

    Add SortedArraySquares

---
+++ src/main/java/com.theta/SortedArraySquares.java
@@ -0,0 +1,27 @@
+package com.theta;
+
+import java.util.Arrays;
+
+public class SortedArraySquares {
+
+  public static int[] sortedSquares(int[] nums) {
+    int left = 0, right = nums.length - 1, index = nums.length - 1;
+    int[] result = new int[index + 1];
+    while(left <= right) {
+      if(Math.abs(nums[left]) > Math.abs(nums[right])) {
+        result[index] = nums[left] * nums[left];
+        left++;
+      } else {
+        result[index] = nums[right] * nums[right];
+        right--;
+      }
+      index--;
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = new int[]{-4, -1, 0, 3, 10};
+    System.out.println(Arrays.toString(sortedSquares(nums)));
+  }
+}
